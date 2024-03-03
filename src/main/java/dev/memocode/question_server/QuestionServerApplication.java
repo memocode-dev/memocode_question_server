09a938650986dbf482d69ac16db688320e8cb466
duswnsxnxn <duswnsxnxn@gmail.com>
2024-03-03 23:09:00 +0900

    init

---
+++ src/main/java/dev/memocode/question_server/QuestionServerApplication.java
@@ -0,0 +1,13 @@
+package dev.memocode.question_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/memocode/question_server/QuestionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.memocode.question_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
