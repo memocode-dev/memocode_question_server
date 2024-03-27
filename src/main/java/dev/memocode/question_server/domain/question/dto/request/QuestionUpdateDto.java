4da4cd19c00a1120aad003142e8e9725421e22a9
duswnsxnxn <duswnsxnxn@gmail.com>
2024-03-27 22:52:39 +0900

    - 유효성 검사 추가

--- src/main/java/dev/memocode/question_server/domain/question/dto/request/QuestionUpdateDto.java
+++ src/main/java/dev/memocode/question_server/domain/question/dto/request/QuestionUpdateDto.java
@@ -1,5 +1,6 @@
 package dev.memocode.question_server.domain.question.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +13,9 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class QuestionUpdateDto {
+    @NotNull
     private UUID questionId;
+    @NotNull
     private UUID userId;
     private String title;
     private String content;
